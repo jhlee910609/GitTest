103d6eb22c6c3dd91a62758ca7bd7fa6c52cdce3
JunHee Lee <jhlee910609@gmail.com>
2017-07-31 18:26:37 +0900

    make 'DetailActivity'.

---
+++ Exam/app/src/main/java/android/mdy/com/exam/DetailActivity.java
@@ -0,0 +1,19 @@
+package android.mdy.com.exam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    String text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        text = "되라.....";
+
+
+    }
+}
--- Exam/app/src/main/java/android/mdy/com/exam/MainActivity.java
+++ Exam/app/src/main/java/android/mdy/com/exam/MainActivity.java
@@ -1,7 +1,9 @@
 package android.mdy.com.exam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button a = new Button(this);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
